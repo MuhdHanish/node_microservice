c2b6216789e9c253da4e6d8a0b24345519e98307
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 14:50:49 +0530

    feat: jwt token added

--- auth/controller/auth.controller.js
+++ auth/controller/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import prisma from "../config/db.config.js";
 
 class AuthController {
@@ -39,8 +40,15 @@ class AuthController {
             if (!user || !bcrypt.compareSync(password, user.password)) {
                 return res.status(401).json({ message: "Invalid credentials!" });
             }
-            const { password: _, ...rest } = user;
-            return res.status(200).json({ message: "Login successful!", user: rest });
+            const payload = {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "1d"
+            })
+            return res.status(200).json({ message: "Login successful!", token: `Bearer ${token}` });
         } catch (error) {
             return res.status(500).json({ message: "Something went wrong, plesae try again." });
         }
