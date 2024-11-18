ae6aaed8d916d5f6d183ede94511a7482b9044a8
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 13:35:06 +0530

    feat: added basic middlewares

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,14 @@
+import cors from "cors";
 import express from "express";
 import { config } from "dotenv";
 
 config();
 
 const app = express();
+f
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get(`/`, (req, res) => {
     return res.json({ message: "Auth Service Wroking" });
