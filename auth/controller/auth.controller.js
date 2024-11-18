198e27278bb5950562912e18aa13eebddc6a6860
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 13:56:55 +0530

    feat: completed register

--- auth/controller/auth.controller.js
+++ auth/controller/auth.controller.js
@@ -1,7 +1,19 @@
+import bcrypt from "bcryptjs";
+import prisma from "../config/db.config.js";
+
 class AuthController {
     static async register(req, res) {
-        const payload = req.body;
-        return res.json({ payload });
+        try {
+            const payload = req.body;
+            const salt = bcrypt.genSaltSync(10);
+            payload.password = bcrypt.hashSync(payload.password, salt);
+            const user = await prisma.user.create({
+                data: payload
+            });
+            return res.status(201).json({ message: "Account created sucessfully!", user });
+        } catch (error) {
+            return res.status(500).json({ message: "Something went wrong, plesae try again." });
+        }
     }
 }
 
