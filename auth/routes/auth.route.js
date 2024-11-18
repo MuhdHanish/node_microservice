0a612e93a5fa159a46214f5c4d27f973b059fbcb
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 15:11:48 +0530

    refactor: auth.router remove /auth prefix

--- auth/routes/auth.route.js
+++ auth/routes/auth.route.js
@@ -4,10 +4,10 @@ import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post(`/auth/register`, AuthController.register);
-router.post(`/auth/login`, AuthController.login);
+router.post(`/register`, AuthController.register);
+router.post(`/login`, AuthController.login);
 
 // Private Route
-router.get(`/auth/user`, authMiddleware, AuthController.user);
+router.get(`/user`, authMiddleware, AuthController.user);
 
 export default router;
