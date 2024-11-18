01182cb20687198fe59f6e086cdc906d8aef9b25
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 15:12:09 +0530

    refactor: removed /api prefix and added /user routes

--- auth/routes/index.js
+++ auth/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import AuthRoutes from "./auth.route.js"
+import UserRoutes from "./user.route.js"
 
 const router = Router();
 
-router.use("/api", AuthRoutes);
+router.use("/auth", AuthRoutes);
+router.use("/user", UserRoutes);
 
 export default router;
