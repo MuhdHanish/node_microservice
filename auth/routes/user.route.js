7c85e4cc60d0fdecaac9369e46486f63111771a6
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 16:44:43 +0530

    feat: added route for getting users by ids

--- auth/routes/user.route.js
+++ auth/routes/user.route.js
@@ -5,5 +5,6 @@ import authMiddleware from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get(`/:id`, authMiddleware, UserController.getUserById);
+router.get(`/ids`, authMiddleware, UserController.getUsersByIds);
 
 export default router;
