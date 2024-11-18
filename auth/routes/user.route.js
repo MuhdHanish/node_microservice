1ddfa96fa5847a4b85cf92ac1fbaa6367661f57f
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 17:20:08 +0530

    refactor: changed user users by ids api end point

--- auth/routes/user.route.js
+++ auth/routes/user.route.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import UserController from "../controller/user.controller.js";
-import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get(`/:id`, UserController.getUserById);
-router.get(`/ids`, UserController.getUsersByIds);
+router.post(`/users-by-ids`, UserController.getUsersByIds);
 
 export default router;
