888ff69a5a41ccff30f8facb3519d98ffc637f89
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-18 15:07:53 +0530

    feat: created user controller

---
+++ auth/controller/user.controller.js
@@ -0,0 +1,20 @@
+import prisma from "../config/db.config.js";
+
+class UserController {
+    static async getUserById(req, res) {
+        try {
+            const { id } = req.params;
+            const user = await prisma.user.findUnique({
+                where: { id }
+            });
+            if (!user) {
+                return res.status(401).json({ message: "User not found." });
+            }
+            return res.json({ user });
+        } catch (error) {
+            return res.status(500).json({ message: "Something went wrong, plesae try again." });
+        }
+    }
+}
+
+export default UserController;
